4f7dc827b42874ab0e9cbfae6f0d1f52675b4130
FloBaur <fbaur@yahoo.de>
2022-05-04 18:50:42 +0200

    Quiz first object

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,19 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int counter = 0;
+  List<Widget> scoreKeeper = [];
+  List<String> questions = [
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  void addIconToScoreKeeper(Widget widget) => {
+        setState(() => {scoreKeeper.add(widget)}),
+        counter++,
+        if (counter == questions.length) {counter = 0}
+      };
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[counter],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -56,11 +69,11 @@ class _QuizPageState extends State<QuizPage> {
               child: Text(
                 'True',
                 style: TextStyle(
-                  color: Colors.white,
                   fontSize: 20.0,
                 ),
               ),
               onPressed: () {
+                addIconToScoreKeeper(Icon(Icons.check, color: Colors.green));
                 //The user picked true.
               },
             ),
@@ -79,19 +92,13 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                addIconToScoreKeeper(Icon(Icons.close, color: Colors.red));
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: scoreKeeper)
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
